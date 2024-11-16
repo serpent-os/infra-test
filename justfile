b5fe364a76712a0d5b06fb23fecb905bc3f59034
Cory Forsstrom <cforsstrom18@gmail.com>
2024-11-15 20:47:36 -0800

    Working legacy infra in docker & bootstrap

--- justfile
+++ justfile
@@ -12,11 +12,22 @@ docker-build target:
 
 [private]
 docker-build-legacy target:
-	@docker build . -t serpentos/{{target}}-legacy --target {{target}} -f Dockerfile-legacy  
+	@docker build . -t serpentos/{{target}}:legacy --target {{target}} -f test/legacy/Dockerfile
 
 # Build docker containers
 build: (docker-build-legacy "summit") (docker-build-legacy "avalanche") (docker-build-legacy "vessel")
 
 # Bring up docker containers
 up: build
-	docker compose up
+	docker compose up --wait --renew-anon-volumes
+
+# Bring down docker containers
+down:
+	docker compose down -v
+
+# Bring up test environment and bootstrap
+bootstrap: up
+	#!/usr/bin/env bash
+	./test/legacy/bootstrap.sh
+	docker compose logs -f
+	docker compose down -v
---
+++ test/legacy/bootstrap.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+SERVICES=(summit vessel avalanche)
+
+declare -A KEYS
+KEYS['summit']=5zIaXc6Cn9qEAk2rNcyu-KDVtdFGcJtx9p2gZdDaxhU
+KEYS['vessel']=qA2GABojHXC7nKo1zCOko2XjktavqGOGUN6Nq9jmUYQ
+KEYS['avalanche']=C64M-DMlib7vl_DAFRPAkKzok6cJ2el1fMxd-LdlGZ0
+
+declare -A PORTS
+PORTS['summit']=5000
+PORTS['vessel']=5001
+PORTS['avalanche']=5002
+
+USER=test
+PASS=test1234
+
+echo "Starting bootstrap"
+
+sleep 2
+
+cookies=$(mktemp)
+
+for service in ${SERVICES[*]}
+do
+  port=${PORTS[$service]}
+
+  case $service in
+    summit)
+      path="setup"
+      ;;
+    *)
+      path="setup/apply"
+      ;;
+  esac
+
+  echo "Creating $service account"
+  curl "http://127.0.0.1:$port/$path" \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    --data-raw "instanceURI=http%3A%2F%2F$service%3A$port&description=&username=$USER&emailAddress=admin%40admin.com&password=$PASS&confirmPassword=$PASS"
+  
+  echo "Logging into $service"
+  curl "http://127.0.0.1:$port/accounts/login" \
+    -b $cookies \
+    -c $cookies \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    --data-raw "username=$USER&password=$PASS"
+done
+
+echo "Adding avalanche"
+curl "http://127.0.0.1:${PORTS['summit']}/api/v1/builders/create" \
+  -b $cookies \
+  -H 'Content-Type: application/json' \
+  --data-raw '{"request":{"id":"avalanche","summary":"Builds stuff","instanceURI":"http://avalanche:'"${PORTS['avalanche']}"'","pubkey":"'"${KEYS['avalanche']}"'","adminName":"admin","adminEmail":"admin@admin.com"}}'
+curl -b $cookies "http://127.0.0.1:${PORTS['avalanche']}/avl/accept/${KEYS['summit']}" 
+
+echo "Adding vessel"
+curl "http://127.0.0.1:${PORTS['summit']}/api/v1/endpoints/create" \
+  -b $cookies \
+  -H 'Content-Type: application/json' \
+  --data-raw '{"request":{"id":"official","summary":"Indexes stuff","instanceURI":"http://vessel:'"${PORTS['vessel']}"'","pubkey":"'"${KEYS['vessel']}"'","adminName":"admin","adminEmail":"admin@admin.com"}}'
+curl -b $cookies "http://127.0.0.1:${PORTS['vessel']}/vsl/accept/${KEYS['summit']}" 
+
+echo "Importing stones"
+curl -b $cookies "http://127.0.0.1:${PORTS['vessel']}/vsl/import?importPath=%2Fimport"
+
+echo "Restarting summit"
+docker compose restart summit
